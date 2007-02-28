86e360cf5cff5abe81a8b172f99bb201c5ea224d
pelaez89 <pelaez89@e3d1f663-132a-0410-a82d-b54ba5e0819a>
2007-02-28 03:17:27 +0000

    Primer subida al repositorio.

---
+++ trunk/ greenpong --username pelaez89/greenPong/src/greenPong/Main.java
@@ -0,0 +1,18 @@
+package greenPong;
+//import java.io.*;
+/*import logica.*; ejemplo para importar modulos de nuestro programa*/
+
+public class Main{
+       
+    public Main() {
+	
+    }
+    
+
+    public static void main(String[] args) {
+    	System.out.println("El programa ha sido corrido");
+
+	
+    }
+    
+}
