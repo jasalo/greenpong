4a27bccb6141d09b84274bd45bc9ad39bbbb8180
pelaez89 <pelaez89@e3d1f663-132a-0410-a82d-b54ba5e0819a>
2007-03-13 02:53:13 +0000

    Se puede jugar con el muose, no con el teclado. Falta alinear el mouse con la barra.

--- greenpong/greenPong/src/greenPong/Box.java
+++ greenpong/greenPong/src/greenPong/Box.java
@@ -6,9 +6,8 @@ package greenPong;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-
 import java.awt.*;
-import java.awt.event.KeyListener;
+import java.awt.Robot;
 
 /**
  * fUNCION SIN METODOS POR AHORA.
@@ -17,7 +16,6 @@ import java.awt.event.KeyListener;
 public class Box extends javax.swing.JPanel {
 	// Definicion de constantes
 	public static final int ANCHO = 446;
-
 	public static final int ALTO = 580;
 
 	// Definicion de las dos barras, una automatica y otra del usuario.
@@ -26,26 +24,39 @@ public class Box extends javax.swing.JPanel {
 	// La bola del juego, no se llama ball para evitar errorer al escribir
 	Ball gameBall;
 	GameLevel nivel;
+	Dimension centro; //centro de la pantalla
 
 	Box(String[] args, javax.swing.JFrame contenedor) {
 		// Args: Util para recibir por consola las imagenes del juego
 		// Se asigna memoria a los controles
+		centro = new Dimension();
 		userBar = new Bar(args[0], this);
 		computerBar = new Bar(args[0], this);
 		gameBall = new Ball(args[1], this);
 		setSize(ANCHO, ALTO); // Numeros tomados del estudio de proporciones
 
-		this.setLayout(null);
+		setLayout(null);
 
-		/* contenedor.getContentPane(). */add(userBar, BorderLayout.NORTH);
-		/* contenedor.getContentPane(). */add(gameBall, BorderLayout.CENTER);
-		/* contenedor.getContentPane(). */add(computerBar, BorderLayout.SOUTH);
+		add(userBar, BorderLayout.NORTH);
+		add(gameBall, BorderLayout.CENTER);
+		add(computerBar, BorderLayout.SOUTH);
 		setBackground(java.awt.Color.RED);
-		/**
-		 * Falta por definir el layout, las posiciones iniciales de los objs.
-		 * 
-		 */
 		ajustarComponentes();	
+		Robot robot;
+		try {
+			robot = new Robot();
+			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+			// Se halla el centro de la pantalla
+			int w = getSize().width;
+			int h = getSize().height;
+			centro.setSize( ((dim.width-w)/2), ((dim.height-h)/2));
+			//Se ubica ventana y mouse al centro;
+			setLocation((int)centro.getWidth(), (int)centro.getHeight() );
+			robot.mouseMove((int)centro.getWidth(), (int)centro.getHeight() );
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
