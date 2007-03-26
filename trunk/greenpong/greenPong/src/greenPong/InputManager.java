1c30298b32e0358741387a32cf5269a69cf0ce07
jasalo <jasalo@e3d1f663-132a-0410-a82d-b54ba5e0819a>
2007-03-26 05:00:40 +0000

    *Eliminados algoritmos comentados que ya no se usaban *Eliminada información mostrada en una ventana aparte para eventos del mouse, eso hizo que el juego no se trabara tanto al mover el mouse, ya es mucho más fluido así.

--- trunk/greenpong/greenPong/src/greenPong/InputManager.java
+++ trunk/greenpong/greenPong/src/greenPong/InputManager.java
@@ -19,8 +19,7 @@ public class InputManager implements java.awt.event.MouseMotionListener{
 	Box contenedor;
 	GameLevel nivel;
 	int px, py; //Posicion previa en x y y.
-	InfoWindow mouseInfo;
-
+	
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
@@ -30,26 +29,10 @@ public class InputManager implements java.awt.event.MouseMotionListener{
 		int x1 = e.getX();
 		int y1 = e.getY();
 		e.translatePoint(0,0);
-		mouseInfo.info("("+x1+","+y1+")");
 
 		if(x1 > 0 && x1 < Box.ANCHO){
-			mouseInfo.info("Mouse dentro de la ventana");
 			userBar.setX(x1);
 		}
-		/*
-		if(x1 - px <0 && x1 == 0)
-		{
-			moveMouse(0,y1);
-			userBar.setX(contenedor.ANCHO-userBar.ANCHO-10);
-		}
-		else if(e.getX() < 74)
-		{
-			userBar.setX(10);
-		}
-		else if(e.getX() <= 309 && e.getX() >= 74)
-		{
-			userBar.setX(x1-64);
-		}*/
 		
 		px = x1;
 		py = y1;
@@ -63,7 +46,6 @@ public class InputManager implements java.awt.event.MouseMotionListener{
 		nivel = nNivel;
 		px=0;
 		py=0;
-		mouseInfo = new InfoWindow("Mouse");
 	}
 	
 	/*public static void moveMouse(int x, int y){
