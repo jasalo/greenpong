b9a97622d08b8b70565df367f22a8fb1c8eb165f
jasalo <jasalo@e3d1f663-132a-0410-a82d-b54ba5e0819a>
2007-03-26 04:52:31 +0000

    Uhhh muchas modificaciones, muchos if's al revés *Se eliminan cosas del tipo if(direccion==ARRIBA) por if(direccion) *Se separa el método perdio() en perdioSubiendo() y perdioBajando() *Algunos if's q estaban en el while del ciclo infinito fueron eliminados, ahora dentro del if con XXX, después de la primera ejecución se alternó el orden de ejecución de los métodos evaluarNormas() y moverBola(), después de una reorganización casi general, y un análisis línea a línea de cada ejecución y modificación, se logró que la bola rebotará. Se eliminaron también muchas líneas usadas para mostrar datos y encontrar errores, esto reduce el tiempo de ejecución de todos los métodos. *La direccion en el método constructor cambió a ABAJO. *Propongo que el atributo velocidad, sea una constante, pues si se cambia su valor, los cálculos son inexactos y casi todo falla; la solución es sólo modificar el atributo que determina cada cuántos milisegundos se debe hacer una ejecución total de la clase Brain, este atributo está en la clase Main como "brainTime"

--- greenpong/greenPong/src/greenPong/Brain.java
+++ greenpong/greenPong/src/greenPong/Brain.java
@@ -1,11 +1,9 @@
 /**
- * Este es el centro del programa, gestioa colisiones y realiza cualquier calculo
+ * Este es el centro del programa, gestiona colisiones y realiza cualquier calculo
  * matemático. Mueve todas las tres partes del programa recibiendo constantemente
  * coordenadas de la clase InputManager y así puede unirse al VTS sin modificaciones
  */
 package greenPong;
-
-import java.awt.event.MouseEvent;
 
 /**
  * @author ubuntu
@@ -27,14 +25,14 @@ public class Brain extends Thread {
 
 	int XXX = 0;
 
-	boolean direccion;
+	boolean direccion = ABAJO;
 
 	InfoWindow info;
 
 	boolean perdio = false;
 
-	int velocidad = 10; // Pixeles del movimiento de la comptuerBar y la bola
-						// por run
+	int velocidad = 1; // Pixeles del movimiento de la computerBar y la bola, por run
+						//Si no es 1 da errores en los cálculos y no rebota la bola
 
 	public Brain(String[] args) {
 		super();
@@ -46,7 +44,6 @@ public class Brain extends Thread {
 		computerBar = contenedor.computerBar;
 		gameBall = contenedor.gameBall;
 		InputManager entrada = new InputManager(userBar, contenedor.nivel);
-		entrada.mouseInfo.setVisible(false);
 		System.out.println("Agregando listener");
 		contenedor.addMouseMotionListener(entrada);
 		direccion = ABAJO;
@@ -60,29 +57,23 @@ public class Brain extends Thread {
 	public void run() {
 		userBar.setLocation(0, userBar.getFinalYPosition());
 		computerBar.centerInX();
-		// computerBar.setX(10);
 		userBar.centerInX();
 		while (1 == 1) {
 			try {
 				sleep(Main.brainTime);
 			} catch (InterruptedException e) {}
 			
-			if (gameBall.getCartesianY() - previousBallY > 0) {
-				direccion = ARRIBA;
-			} else {
-				direccion = ABAJO;
-			}
-			if (XXX != 0) {
-				moverBola();
+			if (XXX > 0) {
 				evaluarNormas();
-				previousBallY = gameBall.getCartesianY();
-			}else{ //Solo corre una vez
-				System.out.println("Primer ejecucion");
-				direccion = ABAJO;
-				previousBallY = gameBall.getCartesianY();
+				moverBola();
+				previousBallY = gameBall.getTopY();
+			}
+			else //Solo corre una vez
+			{
+				System.out.println("Primer ejecucion donde la bola va pa: " + direccion);
+				previousBallY = gameBall.getTopY();
 				moverBola();
 				XXX = 1;
-				
 			}
 			
 
@@ -91,16 +82,16 @@ public class Brain extends Thread {
 	}
 
 	/**
-	 * Este metodo evalua normas del juego o cosas que se tienen que cumplir Si
-	 * no se cumplen genera algo. Usa otro metodos.
-	 * 
+	 * Este metodo evalua normas del juego o cosas que se tienen que cumplir.
+	 * Si no se cumplen genera algo. Usa otro metodos.
 	 */
 	public void evaluarNormas() {
-
-		if (vaPaArriba()) {
-			info.info("Se entro en el ciclo donde la borra vaPaArriba");
-			if (gameBall.getCartesianY() >= kc()) {
-
+		int bolaInf = Box.ALTO - gameBall.getLocation().y - Ball.ALTO - 25; // - 25 pa corregir el desfase raro
+	
+		if (direccion) {
+			
+			if (gameBall.getTopY() == computerBar.getKc()) {
+				
 				int cl = computerBar.leftExtreme();
 				int cr = computerBar.rightExtreme();
 				int bl = gameBall.leftExtreme();
@@ -108,82 +99,71 @@ public class Brain extends Thread {
 
 				if (bl >= cl && bl <= cr) {
 					rebotar();
-					info.info("Moviendo bola");
-					moverBola();
 				} else if (br >= cl && br <= cr) {
 					rebotar();
-					info.info("Moviendo bola");
-					moverBola();
-				} else {
-					perdio();
 				}
-
+			}
+			else if (gameBall.getTopY() > computerBar.getKc())
+			{
+				perdioSubiendo();
 			}
 		}
-
-		if (!vaPaArriba()) {
-			info.info("Se entro en el ciclo donde la borra vaPaAbajo");
-			double bolaInf = gameBall.getLocation().getY()
-					+ gameBall.getHeight();
-			if (bolaInf >= userBar.getLocation().getY()) {
+		
+		if (!direccion) {
+					
+			if ( bolaInf == userBar.getBarY() ) {
+			
 				int ul = userBar.leftExtreme();
 				int ur = userBar.rightExtreme();
 				int bl = gameBall.leftExtreme();
 				int br = gameBall.rightExtreme();
-				if (bl >= ul && bl <= ur) {
+				if (bl >= ul && bl <= ur ) {
 					rebotar();
-					info.info("Moviendo bola");
-					moverBola();
 				} else if (br >= ul && br <= ur) {
 					rebotar();
-					info.info("Moviendo bola");
-					moverBola();
-				} else {
-					perdio();
-
-				}
-
+				}	
+			}
+			else if ( bolaInf < userBar.getBarY() )
+			{
+				perdioBajando();
 			}
 		}
-
 	}
 
 	public void moverBola() {
-		if (direccion = ARRIBA) {
-			info.info("movio paArriba");
+		if (direccion) {
 			gameBall.moveUp(velocidad);
 		} else {
-			info.info("Movio paAbajo");
 			gameBall.moveDown(velocidad);
 		}
-
 	}
 
 	public void moverBarraPC() {
 
 	}
 
-	public boolean vaPaArriba() {
-		if (direccion == ARRIBA) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	public void rebotar() {
-		info.info("La bola reboto. Iba para " + direccion);
-		if (direccion == ARRIBA) {
+		
+		if (direccion) {
 			direccion = ABAJO;
-		} else {
+		} else if (!direccion){
 			direccion = ARRIBA;
 		}
-
+		
 	}
 
-	public void perdio() {
+	public void perdioSubiendo() {
 		perdio = true;
-		info.info("PERDIO");
+		info.info("PERDIO SUBIENDO");
+		info.pause();
+		userBar.info.pause();
+		gameBall.info.pause();
+
+	}
+	
+	public void perdioBajando() {
+		perdio = true;
+		info.info("PERDIO BAJANDO");
 		info.pause();
 		userBar.info.pause();
 		gameBall.info.pause();
@@ -192,24 +172,6 @@ public class Brain extends Thread {
 
 	public void gano() {
 
-	}
-
-	public int kc() {
-		// Retorna en cartesiano
-		int kc = Box.ALTO - ((int) computerBar.getLocation().getY() + Bar.ALTO);
-		if (vaPaArriba()) {
-			info.info("kc=" + kc);
-		}
-		return kc;
-	}
-
-	public int ku() {
-		// Retorna en cartesiano
-		int ku = contenedor.getHeight() - ((int) userBar.getLocation().getY());
-		if (!vaPaArriba()) {
-			info.info("ku=" + ku);
-		}
-		return ku;
 	}
 
 }
